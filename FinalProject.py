e4f9ae1fb8e784bdd7381b08ce12633d17114901
Obley, JP <jpobley@umich.edu>
2013-02-12 11:24:23 -0500

    Lines 55-75: read cities into dictionary

--- FinalProject.py
+++ FinalProject.py
@@ -1,5 +1,9 @@
 from bs4 import BeautifulSoup
-import re, urllib2, json, time
+import re, urllib, urllib2, json, time, pickle
+
+#####################################
+##        Step One - Malcolm       ##
+#####################################
 
 #statesfile = open('state-abbreviations.csv')
 #output1 = open('UFOdata.txt', 'wb')
@@ -43,3 +47,47 @@ import re, urllib2, json, time
 #		output3.write('\n')
 #	except:
 #		continue
+
+#################################
+##        Step Two - JP        ##
+#################################
+
+#read txt file from step one
+f = open('IWantToBelieve.txt', 'rU')
+citystate = f.readlines()
+f.close()
+f = open("text.txt", "wb")
+
+#strip \n character from each line
+i = 0
+while i < len(citystate):
+    citystate[i] = citystate[i].rstrip('\n')
+    i += 1
+
+cities = {}
+regex = re.compile(r'[(),0-9/-?.@#$]')
+for line in citystate:
+  real = "AL"
+  city, state = line.split('\t')
+  if state == real:
+    if not regex.search(city):
+      cities[city] = cities.get(city, 0) + 1
+print cities
+#half = "_Features_20121204.txt"
+
+'''
+address = 'Wyoming (various towns)	WY'
+county = ''
+base_url = 'http://maps.googleapis.com/maps/api/geocode/xml?'
+data = {}
+data['address'] = address
+data['sensor'] = 'false'
+full_url = base_url + urllib.urlencode(data)
+response = urllib2.urlopen(full_url)
+info = response.read()
+root = ET.fromstring(info)
+for each in root.findall('.//address_component'):
+  if each.find('type') is not None:
+    if each.find('type').text == "administrative_area_level_2":
+      zipcode = each.find('long_name').text
+'''
