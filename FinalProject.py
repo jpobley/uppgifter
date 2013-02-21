cb6b93ce09fa2266c3d7b7359d938f90c737465f
Obley, JP <jpobley@umich.edu>
2013-02-20 21:47:54 -0500

    Final version.1 of script and corresponding output

--- FinalProject.py
+++ FinalProject.py
@@ -1,5 +1,6 @@
 #from bs4 import BeautifulSoup
-import re, urllib, urllib2, json, time, pickle, csv, itertools, string
+import re, urllib, urllib2, json, time, pickle, csv, itertools, string, random
+from scipy.stats.stats import pearsonr
 
 #####################################
 ##        Step One - Malcolm       ##
@@ -125,27 +126,25 @@ save.close()
 #######################################
 ##        Step Three - Darren        ##
 #######################################
-
-#load pickle file of sightings dictionary
-#all = pickle.load(open('sightingsStCo.pkl', 'rU'))
-#print all["MI"]["Washtenaw"]
-
 '''
-#read in a bunch of census data into a list of lists, census data is stored in a modified csv file
-#each line corresponds to either a state or a county in the USA and contains (among other items) median income and % pop. in poverty for all ages
-#original file was obtained from http://www.census.gov/did/www/saipe/data/statecounty/data/2010.html
+# Load pickle file of sightings dictionary
+all = pickle.load(open('sightingsStCo.pkl', 'rU'))
+
+# Read in a bunch of census data into a list of lists, census data is stored in a modified csv file
+# Each line corresponds to either a state or a county in the USA and contains (among other items) median income and % pop. in poverty for all ages
+# Original file was obtained from http://www.census.gov/did/www/saipe/data/statecounty/data/2010.html
 census_lol1 = []
 census_csv_file1 = 'est10ALL-edited.csv'
 census_lol1 = list(csv.reader(open(census_csv_file1, 'rU')))
 
-#read in somem more densus data into a second list of lists, census data is stored in a modified csv file
-#each line corresponds to either a state or a county in the USA and contains (among other items) population for that county
-#original file was obtained from http://www.census.gov/popest/data/counties/totals/2011/index.html
+# Read in some more densus data into a second list of lists, census data is stored in a modified csv file
+# Each line corresponds to either a state or a county in the USA and contains (among other items) population for that county
+# Original file was obtained from http://www.census.gov/popest/data/counties/totals/2011/index.html
 census_lol2 = []
 census_csv_file2 = 'CO-EST2011-Alldata-edited.csv'
 census_lol2 = list(csv.reader(open(census_csv_file2, 'rU')))
 
-#compare the two list of lists and append the second one onto the first where the counties match
+# Compare the two list of lists and append the second one onto the first where the counties match
 for i in range(len(census_lol1)):
     for j in range(len(census_lol2)):
         if census_lol1[i][4] != 'State FIPS' and census_lol2[j][3] != 'state':
@@ -153,26 +152,25 @@ for i in range(len(census_lol1)):
                 #census_lol1[i] = census_lol1[i].append(census_lol2[j][7]) #this does not work for me, attempt was to append just the population for that county which is [7]. want to move on so going to just add the whole stupid list whenever the counties match. i am a bad programmer and we can change this later and remove my really long comment yeah that'd probably be good to do i think...
                 census_lol1[i] = census_lol1[i] + census_lol2[j]
 
-#become a pickle farmer, because that's cool, and so that we don't have to complete the 3 previous steps each time we execute this program
+# Pickle the combined census data so we don't have to complete the 3 previous steps each time we execute this program
 save = open('census_combined.pkl', 'wb')
 pickle.dump(census_lol1, save)
 save.close()
-'''
-''''
-#go get that pickled combined census list of lists created previously
+
+# Go get that pickled combined census list of lists we created 
 census = pickle.load(open('census_combined.pkl', 'rU'))
 
-#change the UFO counts into single items lists in order to append corresponding census data for each county
+# Change UFO counts into single item lists so that we can append corresponding census data for each county
 for v1 in all.values():
     v1.update((k,[v]) for k,v in v1.iteritems()) 
 
-#a little string chopper function so that census data county names match those obtained in Step 2
+#A little string chopper function so that census data county names match those obtained in Step 2 (usually we need to remove ' County' from each one, with a few exceptions)
 def chopper(fullcounty, ending): 
     if fullcounty.endswith(ending):
         return fullcounty[:-len(ending)]
     return fullcounty
 
-#execute chopper funciton on all counties
+# Execute chopper funciton on all counties
 for item in census: 
     item[0] = chopper(item[0], ' Parish') #Louisiana why do you have to be different?
 for item in census:
@@ -181,42 +179,91 @@ for item in census:
     #Virginia is also different but it doesn't matter here because each of its cities that are not in a county (yes they have cities that are not in a county) have the word 'county' after them in the census data so it works out...but still...Virginia why do you have to be different?
     item[0] = chopper(item[0], ' County') #remove the ' County' from all counties minus the weirdos above
 
-#horribly imbedded for/if loop with the lone end goal of appending the combined list census data for each county onto the proper county in dictionary of dictionaries of lists, after the number of UFOs
+# Append census data onto state--counties--ufos dictionary of dictionaries of lists
 for state in all:
-    for county in all[state]:
-        for i in range(len(census)):
-            if census[i][1] == state and census[i][0] == county:
-                all[state][county] += census[i]
+  for county in all[state]:
+    for i in range(len(census)):
+      if census[i][1] == state and census[i][0] == county:
+        all[state][county] += census[i]
+for i in range(len(census)):
+  if census[i][1] in all and census[i][0] not in all[census[i][1]]:
+    all[census[i][1]][census[i][0]] = all[census[i][1]].get(census[i][0],[0] + census[i])
 
-# from our final dictionary of dictionaries of lists named all,
-# these are the pertitent items for us to correlate:
-#
-# all[state][county][0] = total reported UFO sitings in county
-# all[state][county][3] = median household income of county
-# all[state][county][4] =  percent (all ages) in poverty in county               
-# all[state][county][32] = population of county
+# Save the new dictionary of states--counties--coordinates with the census data added for each county
+save = open('all_data.pkl', 'wb')
+pickle.dump(all, save)
+save.close()
 
-# richest county in the US via wikipedia table
-print '   --- Brevard County Florida --- '
-print '   Reported UFO sightings: ' + str(all["FL"]["Brevard"][0]) 
-print '   Median Household Income: ' + str(all["FL"]["Brevard"][3])
-print '   Percent (all ages) in Poverty: ' + str(all["FL"]["Brevard"][4])                
-print '   Population: ' + str(all["FL"]["Brevard"][32])
+# From our final dictionary of dictionaries of lists named allData,
+# these are the pertitent items:
+# allData[state][county][0] = total reported UFO sitings in county
+# allData[state][county][3] = median household income of county
+# allData[state][county][4] =  percent (all ages) in poverty in county               
+# allData[state][county][32] = population of county
 
-# poorest county in the US via wikipedia table
-#print 'Buffalo County South Dakota: '
-#print all["SD"]["Buffalo"][0]  
-#print all["SD"]["Buffalo"][3]  
-#print all["SD"]["Buffalo"][4]                  
-#print all["SD"]["Buffalo"][32]  
-#############################################################################################
-### uh oh, Houston we have a problem = where's the data for Buffalo County South Dakota?
-### well, it's in the Census counties (in both Census CSV files) but looks
-### like most likely there were no UFO sightings for this county so it simply
-### does not make it into the dictionary of dictionaries of lists named all
-### Perhaps this error supports our alternative alternative hypothesis:
-### digital divide = no UFO reportings via NUFORC.org for poorest counties
-###
-### as it's 4:54am so I'm going to take a nap maybe when I wake up I'll have a fix -Darren
-#############################################################################################
 '''
+
+allData = pickle.load(open('all_data.pkl', 'rU'))
+
+'''
+# Write the pertitent fields (State, County, UFO Sightings, Median Household Income, Percent In Poverty, Population) to a TSV file, includes header
+with open('ufos_income_poverty_by_county.txt', "w") as myFile:
+    csv.register_dialect("custom", delimiter="\t", skipinitialspace=True)
+    writer = csv.writer(myFile, dialect="custom")
+    writer.writerow(("State", "County", "UFO Sightings", "Median Household Income (USD)", "Percent In Poverty", "Population" )) #create a header
+    for state in allData:
+        for county in allData[state]:
+          if len(allData[state][county]) == 61:
+            ctup = (state, county, allData[state][county][0], allData[state][county][3], allData[state][county][4], allData[state][county][32])
+            writer.writerow(ctup)
+'''
+
+# To run scipy's pearsonr() function you must include two arguements, each must be a 1-dimensional list and the two lists must be the same length
+
+# Create the lists to use in Pearson R correlation calculuations
+ufos = []
+income = []
+poverty = []
+population = []
+
+# Get rid of the commas that appear in some numbers 
+for state in allData:
+    for county in allData[state]:
+        if len(allData[state][county]) == 61:
+            allData[state][county][3] = allData[state][county][3].replace(",", "")
+            allData[state][county][4] = allData[state][county][4].replace(",", "")
+            allData[state][county][32] = allData[state][county][32].replace(",", "")     
+
+# Populate the lists that we will pass to the pearsonr() function
+for state in allData:
+    for county in allData[state]:
+        # for triple if statement below:
+        # 1) make sure county has census values, so 61 items
+        # 2) get rid of those with dot for sightings value
+        # 3) remove outliers from UFO sightings field. There are only outliers on high end (above 5810); low end outlier cutoff is below -5263, which is impossible.
+        if len(allData[state][county]) == 61 and allData[state][county][3] != '.' and allData[state][county][0] < 5993:
+            ufos.append(allData[state][county][0])
+            income.append(int(allData[state][county][3]))
+            poverty.append(float(allData[state][county][4]))
+            population.append(int(allData[state][county][32]))
+    
+# For UFO sightings in each county, calculate Pearson's R values (linear correlation) and associated p-values
+# for Median Household Income, Percent All Ages in Poverty, and Population
+ufos_income = pearsonr(ufos,income)
+ufos_poverty = pearsonr(ufos,poverty)
+ufos_population = pearsonr(ufos,population)
+
+# Print the Pearson's R results
+print ' '
+print ' -------------------------------------------------------- '
+print ' --------- UFO Sightings in the U.S. by county ---------- '
+print ' -------------------------------------------------------- '
+print ' ----- Correlations and P-Vaues (using Pearson R) ------- '
+print ' -------------------------------------------------------- '
+print ' ------------ Correlation Coefficient    P-Value -------- '
+print ' -------------------------------------------------------- '
+print ' Median Income:    %s       %s ' % (ufos_income)
+print ' %% in Poverty:    %s      %s ' % (ufos_poverty)
+print ' Population:       %s      %s ' % (ufos_population)
+print ' -------------------------------------------------------- '
+print ' '
